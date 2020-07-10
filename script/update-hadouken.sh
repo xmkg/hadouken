90048903fd89d2625c73dd860b222e4fa259abde
Mustafa Kemal GILOR <mgilor@nettsi.com>
2020-07-10 12:33:37 +0300

    Added tag check, added poor man's changelog reason: #15

--- script/update-hadouken.sh
+++ script/update-hadouken.sh
@@ -28,7 +28,7 @@ declare -A RP
 # Common headers
 source $SCRIPT_ROOT/common.sh && hadouken.define_relative_paths $SCRIPT_ROOT RP
 
-readonly CURRENT_HADOUKEN_VERSION=$(git -C ${RP["BOILERPLATE"]} tag --points-at)
+CURRENT_HADOUKEN_VERSION=$(git -C ${RP["BOILERPLATE"]} tag --points-at)
 
 case $1 in
     -f|--force)
@@ -42,16 +42,32 @@ esac
 echo "--------------------------------------------------"
 echo "Phase 1: Initiating project update.."
 
+readonly HADOUKEN_REPO_LAST_COMMIT_BEFORE_UPDATE=$(git -C ${RP["BOILERPLATE"]} rev-parse HEAD)
+
 if git -C ${RP["PROJECT"]} submodule update --init --recursive --remote .hadouken/ ; then
   
-  readonly NEW_HADOUKEN_VERSION=$(git -C ${RP["BOILERPLATE"]} tag --points-at)
+  readonly HADOUKEN_REPO_LAST_COMMIT_AFTER_UPDATE=$(git -C ${RP["BOILERPLATE"]} rev-parse HEAD)
+  NEW_HADOUKEN_VERSION=$(git -C ${RP["BOILERPLATE"]} tag --points-at)
   
-  if [ $CURRENT_HADOUKEN_VERSION == $NEW_HADOUKEN_VERSION ]; then
-    echo "Hadouken is already up-to-date ($CURRENT_HADOUKEN_VERSION)"
+  if [ -z "${NEW_HADOUKEN_VERSION}" ] ; then
+    echo "!! Unable to determine new Hadouken version, probably you're following an"
+    echo "!! non-official or experimental branch."
+    NEW_HADOUKEN_VERSION="undefined"
+  elif [ -z "${CURRENT_HADOUKEN_VERSION}"] ; then
+    echo "!! Unable to determine current Hadouken version, probably you're running on a"
+    echo "!! modified, non-official or experimental version."
+    CURRENT_HADOUKEN_VERSION="undefined"
   else
-    echo "Hadouken upgraded from version $CURRENT_HADOUKEN_VERSION to $NEW_HADOUKEN_VERSION"
-    # TODO(mgilor): It might be a good idea to show changelog diff here
+    if [ $CURRENT_HADOUKEN_VERSION == $NEW_HADOUKEN_VERSION ]; then
+      echo "Hadouken is already up-to-date ($CURRENT_HADOUKEN_VERSION)"
+    fi
   fi
+
+  echo "Hadouken upgraded from version $CURRENT_HADOUKEN_VERSION to $NEW_HADOUKEN_VERSION"
+  echo "Changelog:"
+  echo "--------------------------------------------------"
+  git -C ${RP["PROJECT"]} rev-list --ancestry-path $HADOUKEN_REPO_LAST_COMMIT_BEFORE_UPDATE..$HADOUKEN_REPO_LAST_COMMIT_AFTER_UPDATE --oneline --abbrev=1
+  echo "--------------------------------------------------"
   # Execute post-upgrade fix-ups here
 fi
 
