2dfec72df3de325a546551dccc5f88c6f1c02bfe
Mustafa Kemal GILOR <mgilor@nettsi.com>
2020-11-05 13:04:54 +0000

    $project.hadouken_autotargets.test and $project.hadouken_autotargets.benchmark are now depend on conan packages instead of find_package. Hadouken now uses image version 2 for development environment container.

--- cmake/modules/toolconf/GoogleBenchmark.cmake
+++ cmake/modules/toolconf/GoogleBenchmark.cmake
@@ -15,13 +15,39 @@
 # ______________________________________________________
 
 if(${PB_PARENT_PROJECT_NAME_UPPER}_TOOLCONF_USE_GOOGLE_BENCH)
-    message(STATUS "[*] Configuring `googlebench`")
-    
-    find_package(benchmark QUIET REQUIRED)
+
+    # Required conan package and package version
+    set(HADOUKEN_CONAN_GOOGLE_BENCHMARK_PKG_NAME "benchmark")
+    set(HADOUKEN_CONAN_GOOGLE_BENCHMARK_VERSION "1.5.2")
+
+    message(STATUS "[*] Configuring `${HADOUKEN_CONAN_GOOGLE_BENCHMARK_PKG_NAME}` (${HADOUKEN_CONAN_GOOGLE_BENCHMARK_VERSION})")
+
+    # Determine installed "benchmark" package version
+    execute_process(COMMAND /usr/bin/env conan search ${HADOUKEN_CONAN_GOOGLE_BENCHMARK_PKG_NAME} OUTPUT_VARIABLE CONAN_SEARCH_RESULT)
+
+    string(REGEX MATCH "benchmark/(([0-9]+)\\.([0-9]+)\\.([0-9]+))" CONAN_SEARCH_RESULT_VERSION ${CONAN_SEARCH_RESULT})
+
+    if(NOT ${CMAKE_MATCH_COUNT} LESS 4)
+        if(${CMAKE_MATCH_1} STREQUAL ${HADOUKEN_CONAN_GOOGLE_BENCHMARK_VERSION})
+            message(STATUS "\t[+] Conan package  `${HADOUKEN_CONAN_GOOGLE_BENCHMARK_PKG_NAME}/${HADOUKEN_CONAN_GOOGLE_BENCHMARK_VERSION}` present in environment")
+        else()
+            message(STATUS "\t[?] Conan package is present with name `${HADOUKEN_CONAN_GOOGLE_BENCHMARK_PKG_NAME}` but version does not match with required version `${HADOUKEN_CONAN_GOOGLE_BENCHMARK_VERSION}` != `${CMAKE_MATCH_1}`, it will be fetched from remote")
+        endif()
+    else()
+        message(STATUS "\t[-] Conan package  `${HADOUKEN_CONAN_GOOGLE_BENCHMARK_PKG_NAME}/${HADOUKEN_CONAN_GOOGLE_BENCHMARK_VERSION}` is not present in environment, it will be fetched from remote")
+    endif()
+
+    conan_cmake_run(
+        REQUIRES ${HADOUKEN_CONAN_GOOGLE_BENCHMARK_PKG_NAME}/${HADOUKEN_CONAN_GOOGLE_BENCHMARK_VERSION}
+        BASIC_SETUP CMAKE_TARGETS
+        BUILD missing
+        OUTPUT_QUIET
+    )
 
     make_target(
         NAME ${PB_PARENT_PROJECT_NAME}.hadouken_autotargets.benchmark    
         TYPE STATIC SOURCES ${PROJECT_SOURCE_DIR}/.hadouken/cmake/modules/toolconf/GoogleBenchmark.cpp 
-        LINK PUBLIC benchmark::benchmark benchmark::benchmark_main
+        LINK PUBLIC CONAN_PKG::benchmark
     )
+
 endif()
